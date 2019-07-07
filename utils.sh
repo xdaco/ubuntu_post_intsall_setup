82572e8ce8b8d8feb2148c315e91f236dbbd41fc
xdaco <xdaco.xinu@gmail.com>
2019-07-07 11:26:47 +0200

    added spinner

--- utils.sh
+++ utils.sh
@@ -226,3 +226,16 @@ function add_line() {
     grep -F -q "${LINE}" ${FILE} || (echo -e "## ${COMMENT}" >>${FILE} && echo ${LINE} >>${FILE})
 }
 
+show_spinner() {
+    local pid=$!
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
